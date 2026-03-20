06341e79145664b9345b295faeb35af47e4c40c9
Harish Segar <segharish@gmail.com>
2026-03-21 00:04:08 +0100

    adding messaging

--- internal/messaging/messaging.go
+++ internal/messaging/messaging.go
@@ -1,56 +1,6 @@
 package messaging
 
-import (
-	"sync"
-)
-
-type ServiceName uint
-
-const (
-	QueueSize = 1000
-)
-
-type MessageBroadcast struct {
-	blocking bool
-	send     chan<- Message
-}
-
-type MessageQueue <-chan Message
-
-type Messages struct {
-	lock sync.Locker
-
-	subscribers map[ServiceName][]MessageBroadcast
-}
-
-func New() *Messages {
-	return &Messages{lock: &sync.Mutex{}, subscribers: make(map[ServiceName][]MessageBroadcast)}
-}
-
-func (messages *Messages) Subscribe(name ServiceName, blocking bool) MessageQueue {
-	messages.lock.Lock()
-	defer messages.lock.Unlock()
-
-	queue := make(chan Message, QueueSize)
-	if _, exists := messages.subscribers[name]; !exists {
-		messages.subscribers[name] = make([]MessageBroadcast, 0)
-	}
-
-	messages.subscribers[name] = append(messages.subscribers[name], MessageBroadcast{
-		blocking: blocking,
-		send:     queue,
-	})
-	return MessageQueue(queue)
-}
-
-func (messages *Messages) Publish(from ServiceName, message Message) {
-	for _, queue := range messages.subscribers[from] {
-		if queue.blocking || len(queue.send) < QueueSize {
-			queue.send <- message
-		}
-	}
-}
-
+// Message is the interface implemented by all plugin lifecycle event types.
 type Message interface {
 	ismessage()
 }
